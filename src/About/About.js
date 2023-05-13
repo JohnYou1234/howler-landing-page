8b216336a462f585f0c440350f68f2ef51a8eb01
John <yyoungjune@gmail.com>
2023-05-13 16:47:51 -0700

    Finished eslinter

--- src/About/About.js
+++ src/About/About.js
@@ -1,46 +1,56 @@
-import './about.css'
+import './about.css';
 import signup from './img/signup.JPG';
 import create from './img/create.JPG';
 import packs from './img/packs.JPG';
 import search from './img/search.JPG';
 import tile from './img/tile.png';
+import {React} from 'react';
+/**
+ * About component, introduces howler and what it does, also provides images
+ * @return {Element} about div
+ */
 function About() {
-    return (
-        <div id="about">
-            <h2>About Howler</h2>
-            <img className='tile' src={tile} />
-            <p>
-                Howler is a forum-based University of Washington application that is 
-                designed to create a safe space for students to discuss mental health issues and seek support from their peers. 
-                After creating and verifying your UW account, you can start sharing your thoughts and feelings with other Huskies. 
-                Howler is organized into different Packs, which group students of similar interests. 
-                While anyone can browse through the forums of Howler, only verified UW students can post, comment, or make new Packs.
-                This means that any content you see on Howler is verified to be an actual University of Washington student.
-            </p>
-            <div className='f-line images'>
-                <figure>
-                    <img className='site-img' src={signup} />
-                    <figcaption>Easy UW verfified signup process!</figcaption>
-                </figure>
+  return (
+    <div id="about">
+      <h2>About Howler</h2>
+      <img className='tile' src={tile} />
+      <p>
+        Howler is a forum-based University of Washington application that is
+        designed to create a safe space for students to discuss mental health
+         issues and seek support from their peers.After creating and verifying
+         your UW account, you can start sharing your thoughts and feelings
+         with other Huskies. Howler is organized into different Packs,
+         which group students of similar interests. While anyone can browse
+         through the forums of Howler, only verified UW students can post,
+         comment, or make new Packs. This means that any content you see
+         on Howler is verified to be an actual University of Washington student.
+      </p>
+      <div className='f-line images'>
+        <figure>
+          <img className='site-img' src={signup} />
+          <figcaption>Easy UW verfified signup process!</figcaption>
+        </figure>
 
-                <figure>
-                    <img className='site-img' src={create} />
-                    <figcaption>Simplified and customizeable post creations!</figcaption>
-                </figure>
-            </div>
-            <div className='f-line images'>
-                <figure>
-                    <img className='site-img' src={packs} />
-                    <figcaption>View specific groups for specialized information!</figcaption>
-                </figure>
+        <figure>
+          <img className='site-img' src={create} />
+          <figcaption>Simplified and customizeable post creations!</figcaption>
+        </figure>
+      </div>
+      <div className='f-line images'>
+        <figure>
+          <img className='site-img' src={packs} />
+          {/* eslint-disable-next-line max-len */}
+          <figcaption>View specific groups for specialized information!</figcaption>
+        </figure>
 
-                <figure>
-                    <img className='site-img' src={search} />
-                    <figcaption>Convenient search feature to find the perfect post, comment, or board!</figcaption>
-                </figure>
-            </div>
-        </div>
-    )
+        <figure>
+          <img className='site-img' src={search} />
+          {/* eslint-disable-next-line max-len */}
+          <figcaption>Convenient search feature to find the perfect post, comment, or board!</figcaption>
+        </figure>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/Contribute/ContributePage.js
+++ src/Contribute/ContributePage.js
@@ -1,10 +1,20 @@
+/* eslint-disable max-len */
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import './contribute.css'
+import './contribute.css';
+
+/**
+ * Contribute page, where we discuss our open source options
+ * @return {Element} contribute page div
+ */
 function ContributePage() {
   return (
     <div id="contribute-page">
       <h2>Contribute to Howler</h2>
-      <p>At Howler, we believe in the power of open source software and the community that surrounds it. That's why we've decided to make our project open source, and we're inviting you to join us in making it better.</p>
+      <p>At Howler, we believe in the power of open source software
+         and the community that surrounds it. That's why we've decided
+          to make our project open source, and we're inviting you to
+          join us in making it better.</p>
       <h3>Why Contribute?</h3>
       <p>By contributing to Howler, you can:</p>
       <ul>
@@ -15,12 +25,13 @@ function ContributePage() {
       <h3>How to Contribute</h3>
       <p>There are many ways you can contribute to Howler:</p>
       <ul>
-        <li>Report bugs and issues on our <a href="https://github.com/JohnYou1234/capstone-teamU/issues" target='_blank'>GitHub issue tracker</a></li>
+        <li>Report bugs and issues on our <a href="https://github.com/JohnYou1234/capstone-teamU/issues" target='_blank' rel="noreferrer">GitHub issue tracker</a></li>
         <li>Suggest new features and improvements</li>
-        <li>Contribute code by submitting pull requests on our <a href="https://github.com/JohnYou1234/capstone-teamU" target='_blank'>GitHub repository</a></li>
+        <li>Contribute code by submitting pull requests on our <a href="https://github.com/JohnYou1234/capstone-teamU" target='_blank' rel="noreferrer">GitHub repository</a></li>
         <li>Join our community and help us improve our documentation, user interface, and more!</li>
       </ul>
-      <p>If you have any interests or questions, you can email John You, a developer still working on this project, at yyoungjune@gmail.com!</p>
+      <p>If you have any interests or questions, you can email John You,
+         a developer still working on this project, at yyoungjune@gmail.com!</p>
     </div>
   );
 }
